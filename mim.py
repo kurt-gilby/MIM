6b6693a7e095644b4cc24ebba85164202169fabf
kurt-gilby <kurt.gilby@gmail.com>
2023-03-13 16:14:35 +0530

    base on input from user getting the location suggestions

--- mim.py
+++ mim.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import googlemaps as gm
 import os
 import json
+from googlemaps.exceptions import ApiError
 
 def check_file_exist(path, file_name):
     if file_name in os.listdir(path):
@@ -80,6 +81,52 @@ def get_google_maps_api_value(storage_files_exist,use_json_api_key_file,use_gm_a
     else:
         print_google_maps_api_not_found(files_List)
         return"0"
+    
+
+def get_location_suggestions(params):
+    query = params[1].get()
+    gmaps = gm.Client(key=params[0])    
+    try:
+        results = gmaps.places_autocomplete(query)
+        suggestions = [result['description'] for result in results]
+        params[2].delete(0,tk.END)
+        for suggestion in suggestions:
+            params[2].insert(tk.END, suggestion)
+    except ApiError as e:
+        print(f'Error: {e}')
+
+
+
+
+
+def on_entry_change(event,params):
+    get_location_suggestions(params)
+
+
+def load_application(title,width,height,googlemaps_api_key):
+    app = tk.Tk()
+    app.title(title)
+    app.geometry(f"{width}x{height}")
+    location_label = tk.Label(app, text="Enter location:")
+    location_label.pack(anchor=tk.W)
+    location_entry = tk.Entry(app, width=50)
+    location_entry.pack(anchor=tk.W)
+    
+    location_entry_params = []
+    location_entry_params.append(googlemaps_api_key)
+    location_entry_params.append(location_entry)
+    
+
+    suggestion_label = tk.Label(app, text="Pick a Location:")
+    suggestion_label.pack(anchor=tk.W)
+    suggestion_box = tk.Listbox(app, width=50)
+    suggestion_box.pack(anchor=tk.W)
+
+    location_entry_params.append(suggestion_box)
+
+    location_entry.bind("<KeyRelease>", lambda event, params = location_entry_params: on_entry_change(event,params))
+    app.mainloop()
+
 
 
     
@@ -91,6 +138,9 @@ def main():
     API_STORAGE_FILES = [GM_API_KEY_TXT_FILE,API_KEYS_JSON]
     API_KEYS_JSON_GM_KEY ="googlemaps"
     GM_API_VALUE = "MY_GM_API_VALUE"
+    APP_TITLE = "Meet in the Middle"
+    APP_WIDTH = 800
+    APP_WIDTH = 800
     
     does_file_exist_list = check_files_exist(CWD,API_STORAGE_FILES)
     does_file_exist_dict = create_dict_from_list(API_STORAGE_FILES,does_file_exist_list)
@@ -98,8 +148,8 @@ def main():
     should_use_jason_api_key_file = use_api_storage_file(does_file_exist_dict, API_KEYS_JSON)
     use_gm_api_text_file = use_api_storage_file(does_file_exist_dict, GM_API_KEY_TXT_FILE)
     GM_API_VALUE = get_google_maps_api_value(does_api_storage_files_exist,should_use_jason_api_key_file,use_gm_api_text_file,CWD,API_KEYS_JSON,API_KEYS_JSON_GM_KEY,GM_API_KEY_TXT_FILE,API_STORAGE_FILES)
-    
 
+    load_application(APP_TITLE,APP_WIDTH,APP_WIDTH,GM_API_VALUE)
 
     
 if __name__ == "__main__":
